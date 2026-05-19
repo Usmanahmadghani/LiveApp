2a08d8175a32c68e9b5eb028e379276dcbcc5ffb
Usman <umohd5397d@email.com>
2026-05-19 16:16:37 +0530

    home file added

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -11,6 +11,7 @@ import { UsdInrPipesPipe } from './pipes/usd-inr-pipes.pipe'
 import{ReactiveFormsModule} from '@angular/forms';
 import { RedElDirective } from './red-el.directive';
 import { ContactComponent } from './contact/contact.component';
+import { HomeComponent } from './home/home.component';
 
 @NgModule({
   declarations: [
@@ -20,7 +21,8 @@ import { ContactComponent } from './contact/contact.component';
     ChildsComponent,
     UsdInrPipesPipe,
     RedElDirective,
-    ContactComponent
+    ContactComponent,
+    HomeComponent
   ],
   imports: [
     BrowserModule,FormsModule,ReactiveFormsModule,
